51413f2d59133e3912c24a3fec7642fee7b5e102
HamadAwan <=>
2020-12-08 15:13:25 +0500

    radio button

--- src/components/EditForm.js
+++ src/components/EditForm.js
@@ -1,5 +1,10 @@
 import { Button, Paper, TextField } from '@material-ui/core'
 import React, { useState, useEffect } from 'react'
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+
 
 
 const EditForm = props => {
@@ -19,13 +24,22 @@ const EditForm = props => {
   }
 
   return (
-    <Paper style={{display:'flex', width:'80%', height:"40px", padding:'10px', margin:'0 auto',  flexDirection:'row', justifyContent:'space-around'}}>
+    <Paper style={{display:'flex', width:'80%', height:"150px", padding:'10px', margin:'0 auto',  flexDirection:'row', justifyContent:'space-around'}}>
 	
       <label>Name</label>
       <TextField type="text" name="name" value={user.name} onChange={handleInputChange} />
       <label>Gender</label>
-      <TextField type="text" name="gender" value={user.gender} onChange={handleInputChange} />
-      <Button variant="contained" style={{marginLeft:'5px'}} color="primary" 
+      
+      <FormControl component="fieldset">
+				<RadioGroup aria-label="gender" name="gender" value={user.gender} onChange={handleInputChange}>
+					<FormControlLabel value="female" control={<Radio />} label="Female" />
+					<FormControlLabel value="male" control={<Radio />} label="Male" />
+					<FormControlLabel value="other" control={<Radio />} label="Other" />
+				</RadioGroup>
+			</FormControl>
+      {/* <TextField type="text" name="gender" value={user.gender} onChange={handleInputChange} /> */}
+      
+      <Button variant="contained" style={{marginLeft:'5px', height:'50px'}} color="primary" 
         onClick={() => {
           props.setEditing(false)
           props.updateUser(user.id, user)
@@ -33,7 +47,7 @@ const EditForm = props => {
       >
         Update
       </Button>
-      <Button variant="contained"  style={{marginLeft:'5px'}} color="primary"
+      <Button variant="contained"  style={{marginLeft:'5px',height:'50px'}} color="primary"
         onClick={() => {
           props.setEditing(false)
         }}
--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react'
 import { Button, Paper, TextField } from '@material-ui/core'
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
 
 const InputForm = props => {
-	const initialFormState = { id: null, name: '', gender: '' }
+	const initialFormState = { id: null, name: '', gender: 'male' }
 	const [ user, setUser ] = useState(initialFormState)
 
 	const handleInputChange = event => {
@@ -12,12 +16,21 @@ const InputForm = props => {
 	}
 
 	return (
-		<Paper style={{display:'flex', width:'80%', height:"40px", padding:'10px', margin:'0 auto',  flexDirection:'row', justifyContent:'space-around'}}>
+		<Paper style={{display:'flex', width:'80%', height:"150px", padding:'10px', margin:'0 auto',  flexDirection:'row', justifyContent:'space-around'}}>
 			<label>Name:</label>
 			<TextField type="text" name="name" value={user.name} onChange={handleInputChange} />
 			<label>Gender:</label>
-			<TextField type="text" name="gender" value={user.gender} onChange={handleInputChange} />
-			<Button variant="contained" style={{marginLeft:'5px'}} color="primary" 
+
+			<FormControl component="fieldset">
+				<RadioGroup aria-label="gender" name="gender" value={user.gender} onChange={handleInputChange}>
+					<FormControlLabel value="female" control={<Radio />} label="Female" />
+					<FormControlLabel value="male" control={<Radio />} label="Male" />
+					<FormControlLabel value="other" control={<Radio />} label="Other" />
+				</RadioGroup>
+			</FormControl>
+			{/* <TextField type="text" name="gender" value={user.gender} onChange={handleInputChange} /> */}
+			
+			<Button variant="contained" style={{marginLeft:'5px', height:'50px'}} color="primary" 
 				
 				onClick={event => {
 					if (!user.name || !user.gender) return
